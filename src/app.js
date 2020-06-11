03e27b1061f2b279260891662db07decc457b70a
hay-bams <purposenigeria@gmail.com>
2020-06-11 03:14:47 +0100

    initial app

---
+++ src/app.js
@@ -0,0 +1,40 @@
+import express from 'express'
+import dotenv from 'dotenv';
+import { connectDb } from './models'
+import Models from './models';
+import Controllers from './controllers';
+import Services from './services';
+
+
+const Container = require("typedi").Container;
+
+dotenv.config()
+
+const app = express();
+
+const registerDependency =  () => {
+     Container.set('userService', new Services.UserService(Models.User))
+     Container.set('userController', new Controllers.UserController(Container)) 
+}
+
+const registerRoutes = () => {
+    const router = require('./routes').default
+    app.use(router)
+}
+
+connectDb().then(() => {
+    console.log('Database connected')
+})
+
+registerDependency()
+registerRoutes()
+
+const PORT = 3010
+
+app.get('/', function(req, res) {
+    return res.send('Slack API')
+})
+
+app.listen(PORT, function() {
+    console.log(`server is listening on port ${PORT}`)
+})
---
+++ src/models/index.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+import User from './user'; 
+
+export const connectDb = () => {
+    console.log(process.env.DATABASE_URL, '++++++')
+    return mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+}
+
+export default {
+    User
+}
+
